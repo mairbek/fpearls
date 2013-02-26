d0600c1aed29ebf996fe56a65a251ef2e43404ea
Mairbek Khadikov <mkhadikov@gmail.com>
2013-02-26 17:18:31 +0200

    Migrated to loop-recur

--- clj/coursera/src/coursera/recfun.clj
+++ clj/coursera/src/coursera/recfun.clj
@@ -8,17 +8,15 @@
 (def rb \))
 
 (defn balance? [s] 
-	(let [parentheses 
-		(fn [st count] 
-			(cond
-				(not (seq st)) count
-				(= -1 count) -1
-				(= lb (first st)) (recur (rest st) (inc count)) 
-				(= rb (first st)) (recur (rest st) (dec count))
-				:else (recur (rest st) count)
-				))]
-		(= 0 (parentheses s 0))))
-
+	(let [p (loop [s s count 0] 
+				(cond
+					(not (seq s)) count
+					(= -1 count) -1
+					(= lb (first s)) (recur (rest s) (inc count)) 
+					(= rb (first s)) (recur (rest s) (dec count))
+					:else (recur (rest s) count)
+				))] 
+		(= 0 p)))
 
 (defn count-change [money coins] 
 	(cond 
