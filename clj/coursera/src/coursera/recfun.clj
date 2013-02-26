294e7d90fd59d87bb174a2ef34a894f0300fb81e
Mairbek Khadikov <mkhadikov@gmail.com>
2013-02-26 11:39:21 +0200

    Migrated parenthes balance check

--- clj/coursera/src/coursera/recfun.clj
+++ clj/coursera/src/coursera/recfun.clj
@@ -2,3 +2,19 @@
 
 (defn pascal [c r] 
 	(if (or (= 0 c) (= c r)) 1 (+ (pascal (dec c) (dec r)) (pascal c (dec r)))))
+
+(def lb \()
+(def rb \))
+
+(defn balance? [s] 
+	(let [parenthes 
+		(fn [st count] 
+			(cond
+				(not (seq st)) count
+				(= -1 count) -1
+				(= lb (first st)) (recur (rest st) (inc count)) 
+				(= rb (first st)) (recur (rest st) (dec count))
+				:else (recur (rest st) count)
+				))]
+		(= 0 (parenthes s 0))))
+
--- clj/coursera/test/coursera/recfun_test.clj
+++ clj/coursera/test/coursera/recfun_test.clj
@@ -11,3 +11,13 @@
     (is (= 3 (pascal 1 3))))
   (testing "col=2, row=4"
     (is (= 6 (pascal 2 4)))))
+
+(deftest balance-test
+	(testing "'(if (zero? x) max (/ 1 x))' is balanced."
+		(is (balance? "(if (zero? x) max (/ 1 x))")))
+	(testing "'I told him ...' is balanced."
+		(is (balance? "I told him (that it's not (yet) done).\n(But he wasn't listening)")))
+	(testing "':-)' is unbalanced."
+		(is (not (balance? ":-)"))))
+	(testing "Counting is not enough"
+		(is (not (balance? "())(")))))
